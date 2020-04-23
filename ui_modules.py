345d42985a49c0d488c24f781fce0f994fe74236
dnavarro <the.electric.me@gmail.com>
2020-04-23 15:25:17 -0300

    typo

--- ui_modules.py
+++ ui_modules.py
@@ -16,7 +16,7 @@ def representative(fields):
 
 def similar(node,substring_length = 3, max_examples = 4):
 	effective_length = min(len(node),substring_length)
-	nodes = db.query_nodes()
+	nodes = db.list_nodes()
 	candidates = [n for n in nodes if n[:effective_length]==node[:effective_length] and n!=node]
 	effective_num_candidates = min(len(candidates),max_examples)
 	return list(set(np.random.choice(candidates,effective_num_candidates)))
