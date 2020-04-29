0dbdc7f4854159ac426a11fd35bec239408247b8
dnavarro <the.electric.me@gmail.com>
2020-04-29 12:45:58 -0300

    color! smaller blotches

--- viz.py
+++ viz.py
@@ -14,9 +14,10 @@ def draw_bw(G, pos_fun=nx.spring_layout):
 def draw_color(G, pos_fun=nx.spring_layout, cmap=cm.bone):
 	pos = pos_fun(G)
 	font_size = 11 if G.number_of_nodes()<50 else 9
-	alpha = 1
+	alpha = 0.5
+	node_size = 75
 	potential = phys.graph_potential(G)
 	color = (potential - potential.min())/(potential.max() - potential.min())
-	nx.draw(G,pos=pos,with_labels=True, node_color=color,font_size=font_size,width=0.2,alpha=alpha)
+	nx.draw(G,pos=pos,with_labels=True, node_color=color, node_size = node_size,font_size=font_size,width=0.2,alpha=alpha)
 	st.pyplot()
 
