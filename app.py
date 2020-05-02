3679b2b0752b892259aa629aa4b7e3e75d3f5515
dnavarro <the.electric.me@gmail.com>
2020-05-01 23:52:08 -0300

    port to sqlite backedn

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import layout
 import db
 import ui_modules as ui
 import viz, physical
+from db import nc
 
 cmap = "PuOr_r"
-db.calculate_potential()
+with nc() as conn: db.calculate_energy(conn)
 node_mode = "Nodes"
 conn_mode = "Connections"
 nonn_mode = "Trailing"
@@ -25,24 +26,26 @@ if op_mode == node_mode:
 	node = st.text_input('Enter new node name')
 	add_button, del_button = ui.add_del()
 	if node and add_button: 
-		found = db.write_node(node)
+		with nc() as conn: found = db.write_node(conn,node)
 		ui.if_confirm(found,"Name already exists")
 	if node and del_button:
-		found = db.del_node(node)
+		with nc() as conn: found = db.del_node(conn,node)
 		ui.if_confirm(found)
 elif op_mode == dyad_mode:
 	node_1 = st.text_input('Enter new node 1 name')
 	node_2 = st.text_input('Enter new node 2 name')
 	add_button, del_button = ui.add_del()
 	if node_1 and node_2 and add_button:
-		db.write_node(node_1)
-		db.write_node(node_2)
-		db.write_edge(node_1, node_2)
+		with nc() as conn:
+			db.write_node(conn,node_1)
+			db.write_node(conn,node_2)
+			db.write_edge(conn,node_1, node_2)
 		ui.confirm()
 	if node_1 and node_2 and del_button:
-		db.del_edge(node_1,node_2)
-		db.del_node(node_1)
-		db.del_node(node_2)
+		with nc() as conn:
+			db.del_edge(conn,node_1,node_2)
+			db.del_node(conn,node_1)
+			db.del_node(conn,node_2)
 		ui.confirm()
 elif op_mode == triad_mode:
 	parent = st.text_input('Enter head node name')
@@ -50,43 +53,46 @@ elif op_mode == triad_mode:
 	right = st.text_input('Enter right child name')
 	add_button, del_button = ui.add_del()
 	if parent and left and right and add_button:
-		db.write_node(parent)
-		db.write_node(left)
-		db.write_node(right)
-		db.write_edge(parent,left)
-		db.write_edge(parent,right)
+		with nc() as conn:
+			db.write_node(conn,parent)
+			db.write_node(conn,left)
+			db.write_node(conn,right)
+			db.write_edge(conn,parent,left)
+			db.write_edge(conn,parent,right)
 	if parent and left and right and del_button:
-		db.del_node(parent)
-		db.del_node(left)
-		db.del_node(right)
-		db.del_edge(parent,left)
-		db.del_edge(parent,right)
+		with nc() as conn:
+			db.del_node(conn, parent)
+			db.del_node(conn, left)
+			db.del_node(conn, right)
+			db.del_edge(conn, parent,left)
+			db.del_edge(conn, parent,right)
 
 elif op_mode == conn_mode:
-	nodes = db.list_nodes()
+	with nc() as conn: nodes = db.list_nodes(conn)
 	node_1 = ui.known_field_input("Source","jazz")
 	node_2 = ui.known_field_input("Target","espionage")
 	add_button, del_button = ui.add_del()
 	if node_1 and node_2 and add_button: 
-		db.write_edge(node_1,node_2)
+		with nc() as conn: db.write_edge(conn, node_1,node_2)
 		ui.confirm()
 	if node_1 and node_2 and del_button:
 		found = True
 		while found:
-			found = db.del_edge(node_1, node_2)
+			with nc() as conn: found = db.del_edge(conn,node_1, node_2)
 		st.write("(Edge deleted.)")
 		ui.confirm()
 elif op_mode == nonn_mode:
-	nodes = db.list_nodes()
+	with nc() as conn: nodes = db.list_nodes(conn)
 	node_1 = st.selectbox("Existing",nodes,index=nodes.index("jazz"))
 	node_2 = st.text_input('New')
 
 	nonn_button = st.button("Add and connect")
 	if node_1 and node_2 and nonn_button:
-		db.write_node(node_2); ui.confirm()
-		db.write_edge(node_1,node_2); ui.confirm()
+		with nc()as conn:
+			db.write_node(conn,node_2); ui.confirm()
+			db.write_edge(conn,node_1,node_2); ui.confirm()
 elif op_mode == merge_mode:
-	nodes = db.list_nodes()
+	with nc() as conn: nodes = db.list_nodes(conn)
 	u,v = 1,3
 	node_1 = st.selectbox("Source 1",nodes,index=u)
 	node_2 = st.selectbox("Source 2",nodes,index=v)
@@ -98,7 +104,7 @@ elif op_mode == merge_mode:
 		new_node = st.text_input("New node", value=defval)
 		merge_button = st.button("Merge")
 		if merge_button:
-			db.merge_nodes(node_1,node_2, new_node)
+			with nc() as conn: db.merge_nodes(conn,node_1,node_2, new_node)
 			ui.confirm()
 
 elif op_mode == view_mode:
@@ -113,14 +119,15 @@ elif op_mode == view_mode:
 		center = None
 		radius = None
 	else:
-		fields = db.list_nodes()
+		with nc() as conn: fields = db.list_nodes(conn)
 		u = fields.index("jazz")
 		center = st.selectbox("Choose nodes",fields,index = u)
 		radius = st.number_input("Radius",value=1)
 
 
-	G = db.graph(center = center, radius = radius)
-	pot = db.read_potential(G)
+	with nc() as conn:
+		G = db.graph(conn,center = center, radius = radius)
+		pot = db.read_energy(conn,G)
 	if color:
 		viz.draw_color(G,pot = pot, pos_fun = nx.spring_layout if algo==algo0 else nx.kamada_kawai_layout, cmap = cmap)	
 	else:
