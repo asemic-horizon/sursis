51a277725016af5930155d3a3643c61a4c7f5342
dnavarro <the.electric.me@gmail.com>
2020-05-03 15:41:22 -0300

    sql refactor

--- graph_physics.py
+++ graph_physics.py
@@ -9,9 +9,9 @@ def update_physics(conn):
     mass, energy = phys.mass(G), phys.energy(G)
     values = zip(G.nodes(),mass,energy)
     for node, mass, energy in values:
-        _ = db.run_sql(conn,\
+        db.run_sql(conn,\
             "UPDATE nodes SET mass = ? WHERE name = ? LIMIT 1", mass, node).lastrowid
-        _ = db.run_sql(conn,\
+        db.run_sql(conn,\
             "UPDATE nodes SET energy = ? WHERE name = ? LIMIT 1", energy, node).lastrowid
 
     # EDGES
@@ -19,14 +19,15 @@ def update_physics(conn):
     mass, energy = phys.mass(H), phys.energy(H)
     values = [(u,v,m,p) for (u,v),m, p in zip(H.edges(),mass,energy)]
     for u, v,  mass, energy in values:
-        _ = db.run_sql(conn,\
-            "UPDATE nodes SET mass = ? WHERE left  = ? and right = ?", mass, u, v).lastrowid
-        _ = db.run_sql(conn,\
-            "UPDATE nodes SET mass = ? WHERE left  = ? and right = ?", mass, v, u).lastrowid
-        _ = db.run_sql(conn,\
-            "UPDATE nodes SET energy = ? WHERE left  = ? and right = ?", energy, u, v).lastrowid
-        _ = db.run_sql(conn,\
-            "UPDATE nodes SET energy = ? WHERE left  = ? and right = ?", energy, v, u).lastrowid
+        id_1, id_2 = get_node_id(u),get_node_id(v)
+        db.run_sql(conn,\
+            "UPDATE nodes SET mass = ? WHERE left  = ? and right = ?", mass, id_1, id_2).lastrowid
+        db.run_sql(conn,\
+            "UPDATE nodes SET mass = ? WHERE left  = ? and right = ?", mass, id_2, id_1).lastrowid
+        db.run_sql(conn,\
+            "UPDATE nodes SET energy = ? WHERE left  = ? and right = ?", energy, id_1, id_2).lastrowid
+        db.run_sql(conn,\
+            "UPDATE nodes SET energy = ? WHERE left  = ? and right = ?", energy, id_2, id_1).lastrowid
 
 
 def read_node_prop(conn,subgraph,prop="energy"):
