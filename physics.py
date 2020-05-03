4f858e1806cb0f2df94ddaa3bc6bd04f96d5419c
dnavarro <the.electric.me@gmail.com>
2020-05-03 15:18:21 -0300

    renames

---
+++ physics.py
@@ -0,0 +1,27 @@
+import networkx as nx 
+import scipy
+import numpy as np
+from joblib import Memory
+
+memory = Memory("./cache")
+
+@memory.cache
+def mass(graph)
+	metric = nx.betweenness_centrality(graph)
+	metric = np.array(list(dict(metric).values()))
+	return metric
+
+
+@memory.cache
+def potential(graph : nx.Graph,mass : np.ndarray):
+	rho = mass.reshape(1,-1)
+	L = nx.laplacian_matrix(graph)
+	L_inv = scipy.linalg.pinv(L.todense())
+	return L_inv.dot(mass)
+
+def energy(graph : nx.Graph):
+	return rescale(potential(graph,mass(graph)))
+
+def rescale(y : np.ndarray):
+	t = (y - y.mean())/y.std()
+	return t/4
