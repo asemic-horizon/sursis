bb86e25c1b7cdbbe0841d832d75f39c120878836
dnavarro <the.electric.me@gmail.com>
2020-05-07 10:01:54 -0300

    sparse least squares potential + disable caching until graph grows

--- physics.py
+++ physics.py
@@ -13,11 +13,18 @@ def mass(graph):
 
 
 @memory.cache
-def potential(graph : nx.Graph,mass : np.ndarray):
+def penrose_potential(graph : nx.Graph,mass : np.ndarray):
 	rho = mass.reshape(1,-1)
 	L = nx.laplacian_matrix(graph)
 	L_inv = scipy.linalg.pinv(L.todense())
 	return L_inv.dot(mass)
+
+@memory.cache
+def potential(graph: nx.Graph, mass : np.ndarray):
+	rho = mass.reshape(-1,1)
+	L = nx.laplacian_matrix(graph)
+	sol = scipy.sparse.linalg.lsmr(L, rho)
+	return sol[0]
 
 def energy(graph : nx.Graph):
 	return rescale(potential(graph,mass(graph)))
