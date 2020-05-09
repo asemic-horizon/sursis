e51c7d44c5bfff8fcc7bebe75cf36db56c991d66
dnavarro <the.electric.me@gmail.com>
2020-05-09 00:32:40 -0300

    s

--- graph_stats.py
+++ graph_stats.py
@@ -12,11 +12,10 @@ def plot_degree_distribution(graph):
 	degree_sequence = sorted([d for n, d in graph.degree()], reverse=True)  # degree sequence
 	degreeCount = collections.Counter(degree_sequence)
 	deg, cnt = zip(*degreeCount.items())
-	st.write(deg,cnt)
 	popt, _ = curve_fit(f=power_law,xdata=deg,ydata=cnt)
 	k, slope = tuple(popt)
 	plt.scatter(deg,cnt)
-	plt.plot(power_law,deg,k,slope)
+	plt.plot(deg,power_law(deg,k,slope))
 	plt.yscale('log')
 	plt.grid(True)
 	plt.title("Degree distribution")
