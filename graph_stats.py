17eb9675dfa405353dd40e3263a4cafc2d30bb4e
dnavarro <the.electric.me@gmail.com>
2020-05-09 00:19:51 -0300

    fs

--- graph_stats.py
+++ graph_stats.py
@@ -7,7 +7,7 @@ def plot_degree_distribution(graph):
 	degreeCount = collections.Counter(degree_sequence)
 	deg, cnt = zip(*degreeCount.items())
 	plt.scatter(deg,cnt)
-	plt.set_yaxis('log')
+	plt.yscale('log')
 	st.pyplot()
 def stats_view(graph):
 
