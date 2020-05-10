23e15b0d370d4f7e11782ef7509c3ad38b1a4b29
dnavarro <the.electric.me@gmail.com>
2020-05-10 15:32:23 -0300

    ow

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ elif op_mode == view_mode:
 	full_graph = st.checkbox("Full graph",value=False)
 	with nc() as conn: 
 		chem.update_physics(conn)
-		if full_graph:
-			center, radius = None
+		if full_graph:read_node_
+			center, radius = None, None
 		if not full_graph:
 			fields = db.list_nodes(conn)
 			u = db.count_nodes(conn)-1
