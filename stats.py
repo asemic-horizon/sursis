42c374d2261ac085f1f9de5f7779f36547b40156
dnavarro <the.electric.me@gmail.com>
2020-05-11 23:46:10 -0300

    refactor

---
+++ stats.py
@@ -0,0 +1,17 @@
+import networkx as nx 
+import collections
+import numpy as np
+import physics as phys
+import graph_physics as chem
+import scipy
+
+def degree_distribution(graph):
+	degree_sequence = sorted([d for n, d in graph.degree()], reverse=True)  # degree sequence
+	degreeCount = collections.Counter(degree_sequence)
+	deg, cnt = zip(*degreeCount.items())
+	return deg, cnt 
+
+def spectrum(graph):
+	L = nx.laplacian_matrix(graph)
+	eigvals = scipy.sparse.linalg.eigsh(L,k=L.shape[0], return_eigenvectors = False)
+	return eigvals
