a3b3ba01f4402fafed7ce6f7054e5b97216c95a9
dnavarro <the.electric.me@gmail.com>
2020-05-13 10:09:37 -0300

    meta packaging stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setuptools
+
+setup(name="sursis",
+	  version="0.1",
+	  description="Personal notebook network",
+	  url="https://github.com/asemic-horizon/sursis",
+	  author="asemic horizon",
+	  author_email="undisclosed",
+	  license="MIT",
+	  packages=["sursis"],
+	  zip_safe = False)
+
