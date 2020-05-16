67c57b2185782c23f1c561111ef8e30d0f7a8784
dnavarro <the.electric.me@gmail.com>
2020-05-16 14:07:21 -0300

    _____

--- sursis/backend/physics.py
+++ sursis/backend/physics.py
@@ -55,7 +55,7 @@ def potential(graph: nx.Graph, mass: np.ndarray, boundary_value = 0.0, bracket=(
 		-rho,
 		bounds=bounds,
 		max_iter = len(rho)*len(rho))
-	logging.info("Optimality: " + sol.mesage)
+	logging.info("Optimality: " + sol.message)
 	logging.info(str(test_boundary(graph, sol.x)))
 	return sol.x
 
