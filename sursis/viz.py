1db780a69d60c90f8b8f8c6f802624db9c7c753f
dnavarro <the.electric.me@gmail.com>
2020-05-16 22:26:07 -0300

    _

--- sursis/viz.py
+++ sursis/viz.py
@@ -62,7 +62,7 @@ def draw(G, conn, labels = True, cmap = "terrain_r", pos_fun=nx.kamada_kawai_lay
         energy = np.array(chem.read_node_prop(conn,G,"energy"))
         mass = np.array(chem.read_node_prop(conn,G,"mass"))
         minm, maxm, avgm, medm = chem.prop_bounds(conn,prop="mass")
-        multiplier = -1300/np.log10(medm)
+        multiplier = -1600/np.log10(medm)
         node_size = 10+multiplier*mass
         minv, maxv, avgv, medv = chem.prop_bounds(conn,slices=10)
         #pot = np.exp(energy)
