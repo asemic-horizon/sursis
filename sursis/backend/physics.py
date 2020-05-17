fee6f4d3ce8b1d75cdee53f233e2246aa142a2a6
dnavarro <the.electric.me@gmail.com>
2020-05-16 22:50:04 -0300

    ffly

--- sursis/backend/physics.py
+++ sursis/backend/physics.py
@@ -48,7 +48,7 @@ def least_squares_potential(graph: nx.Graph, mass : np.ndarray):
 	return sol[0]
 
 #@mem.cache
-def potential(graph: nx.Graph, mass: np.ndarray, boundary_value = 0.0, bracket=(-np.inf,np.inf)):
+def potential(graph: nx.Graph, mass: np.ndarray, boundary_value = 1.0, bracket=(-np.inf,np.inf)):
 	rho = mass.reshape(-1,)
 	L = nx.laplacian_matrix(graph)
 	bounds = boundary_condition(graph, boundary_value, *bracket)
