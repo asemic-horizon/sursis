4c0a4584c6622d225619c7cd1e55b953f71106e5
dnavarro <the.electric.me@gmail.com>
2020-05-18 11:11:15 -0300

    [0]

--- sursis/graph_views.py
+++ sursis/graph_views.py
@@ -69,7 +69,7 @@ def energy(graph,conn):
 	plt.axhline(0.5,c='g',linestyle="dotted")
 	plt.axvline(np.median(grav),c='g',linestyle="dotted")
 	plt.axvline(np.mean(grav),c='r',linestyle="dashed")
-	plx.axvline(0,c='y',linestyle="dotted")
+	plt.axvline(0,c='y',linestyle="dotted")
 
 #	plt.axhline(np.where(grav==np.mean(grav))[0],c="r",linestyle="dashed")
 	plt.title("Cumulative energy")
