5c7b240b8a2c0d91e2f668d3d86899998313cbad
dnavarro <the.electric.me@gmail.com>
2020-05-18 11:39:28 -0300

    extraphysics

--- sursis/backend/physics.py
+++ sursis/backend/physics.py
@@ -6,25 +6,21 @@ import scipy
 import numpy as np
 from joblib import Memory
 mem = Memory('./cache')
-def isolates(graph):
-	 return [n for n,f in enumerate(graph.nodes) if graph.degree[f]==0]
 
-def boundary(graph):
-	 return [n for n,f in enumerate(graph.nodes) if graph.degree[f]==1]
+def boundary(graph,thresh):
+	 return [n for n,f in enumerate(graph.nodes) if graph.degree[f]==thresh]
 
-def weaklink(graph):
-	 return [n for n,f in enumerate(graph.nodes) if graph.degree[f]==2]
+def test_boundary(graph,vector,thresh):
+	return np.median(vector[boundary(graph,thresh)])
 
-def test_boundary(graph,vector):
-	return np.median(vector[boundary(graph)])
-
-def boundary_condition(graph, value = 0.0, lower = -np.inf, higher = np.inf, eps = 1e-10):
+def boundary_condition(graph, value = 0.0, thresh=2, lower = -np.inf, higher = np.inf, eps = 1e-10):
 	n = graph.number_of_nodes()
-	gb = boundary(graph)
 	lb = np.full((n,),lower)
 	ub = np.full((n,),higher)
-	lb[gb] = value-eps
-	ub[gb] = value+eps
+	for i in range(1+thresh):
+		gb = boundary(graph)
+		lb[gb] = value-eps
+		ub[gb] = value+eps
 	#print(lb)
 	return (lb,ub)
 
